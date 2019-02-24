63dcfd4734a8563272d6b8f0049548b24c92f016
Jomon Johnson <jomon.entero@gmail.com>
2019-02-24 17:02:02 +0530

    init

---
+++ src/config/site.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jomon
+ * Date: 9/6/18
+ * Time: 4:20 PM
+ */
+
+return [
+    'runtimePath' => '@root/runtime/site',
+    'defaultRoute' => 'site/index',
+    'controllerNamespace' => '\eii\site\controllers',
+    'components' => [
+        'i18n' => [
+            'translations' => [
+                'entero:site' => [
+                    'class' => \yii\i18n\PhpMessageSource::class,
+                    'basePath' => '@eii/site/messages',
+                ],
+            ],
+        ],
+        'view' => [
+            'theme' => [
+                'pathMap' => [
+                    '@app/views' => [
+                        '@eii/site/views',
+                    ],
+                ],
+            ],
+        ],
+    ],
+];
