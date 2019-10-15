916193f0cb5a00184b96b51ac674fd214c0f2e79
Jomon Johnson <jomon.entero@gmail.com>
2019-10-15 17:13:35 +0530

    init

---
+++ src/autocompletion.php
@@ -0,0 +1,11 @@
+<?php
+
+use codexten\yii\sitemap\components\Sitemap;
+use codexten\yii\web\UrlManager;
+
+/**
+ * @property UrlManager $urlManagerSite
+ */
+abstract class BaseApplication extends yii\base\Application
+{
+}
